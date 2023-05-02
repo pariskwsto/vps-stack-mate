9c4e9767d0d69d91001e0f9c22efe4c75336cec4
Paris Kostopoulos <pariskwsto@gmail.com>
2023-05-02 13:27:16 +0300

    feat(reload-service): improve script with services list

--- scripts/reload/service.sh
+++ scripts/reload/service.sh
@@ -4,8 +4,18 @@ source utils/logger.sh
 
 logger CMD "Run 'reload-service' command"
 
-logger DECISION "Enter the name of the service you need to reload/recreate: "
-read -p "" SERVICE_NAME
+services=$(docker-compose config --services)
+
+PS3="Enter the number of the service you want to reload: "
+select SERVICE_NAME in $services
+do
+  if [[ -n "$SERVICE_NAME" ]]
+  then
+    break
+  fi
+done
+
+logger DECISION "Reloading/recreating service $SERVICE_NAME"
 
 docker-compose pull $SERVICE_NAME
 docker-compose up -d --force-recreate $SERVICE_NAME
